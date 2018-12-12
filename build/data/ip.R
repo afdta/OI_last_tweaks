60ffa143005809d74796a2f883000ac5d46c4368
Alec Friedhoff <afriedhoff@gmail.com>
2018-12-12 17:25:33 -0500

    moved all data files to build/data

--- build/data/ip.R
+++ build/data/ip.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(jsonlite)
+library(readxl)
 
 #From Chad:
 #All the data will be uploaded here: 
@@ -25,23 +26,28 @@ dir_download <- "https://app.box.com/v/BMPPOI-MetroJobShares"
 #weight – Raw person weight (number of employed in that starting occ meeting personal descriptors as implied by ACS data) – ignore this
 
 occs <- read_csv("/home/alec/Projects/Brookings/opportunity-industries/build/data/metro_job_quality/met_opptyocc2017j.csv") %>% 
-        filter(level==3) %>% mutate(occ=substr(sub("-","",soc_code), 1, 3))
+        filter(level==2) %>% mutate(occ=substr(sub("-","",soc_code), 1, 2))
 
-occs2 <- occs %>% group_by(cbsa_code, CBSA_Title) %>% mutate(shtot=total/sum(total))
+occs2 <- occs %>% group_by(cbsa_code, CBSA_Title, metdiv_title) %>% mutate(shtot=total/sum(total))
+
+occs2 %>% summarise(shtotal = sum(shtot))
 
 ind0 <- read_csv("/home/alec/Projects/Brookings/opportunity-industries/build/data/metro_job_quality/met_opptyind2017s.csv") 
 
-ind <- ind0 %>% mutate(hi=hi_good_share + hi_promising_share) %>% 
+ind <- ind0 %>% mutate(hi=hi_good_share + hi_promising_share) %>% filter(is.na(metdiv_code)) %>%
                 select(cbsa=cbsa_code, naics, ind=industry, g=good_share, p=promising_share, hi, o=other_share, u=undefined, l=level)
 
-ind2 <- ind %>% filter(l==2 | l==1, is.na(metdiv_code))
+ind2 <- ind %>% filter(l==2 | l==1)
 
 #duplicates?
 counts <- ind2 %>% group_by(cbsa) %>% summarise(n=n())
+maxes <- ind2 %>% filter(is.na(naics)) %>% summarise(max=max(g, p, hi, o, u))
 
-ind3 <- ind %>% filter(l==3 | l==1)
+#indkeys <- ind2 %>% select(naics, ind) %>% unique() %>% spread(naics, ind) %>% unbox()
+indkeys <- read_xlsx("/home/alec/Projects/Brookings/opportunity-industries/build/data/Shortened titles.xlsx", sheet="Industries") %>%
+            mutate(naics = sub("-.*$", "", Code)) %>% mutate(nchar = nchar(naics)) %>% select(naics, ind=`Short title`) %>% 
+            spread(naics, ind) %>% unbox()
 
-indkeys <- ind2 %>% select(naics, ind) %>% unique() %>% spread(naics, ind) %>% unbox()
 metros <- ind0 %>% select(code=cbsa_code, name=CBSA_Title) %>% unique()
 
 indjson <- toJSON(ind2 %>% select(-ind, -l) %>% as.data.frame() %>% split(.$cbsa) %>% lapply(function(d){return(d %>% select(-cbsa, -u))}), na="null", digits=5, pretty=TRUE)
@@ -56,8 +62,8 @@ flows0 <- read_csv("/home/alec/Projects/Brookings/opportunity-industries/build/d
 counts <- flows0 %>% group_by(cbsa) %>% summarise(n=n())
 
 #occupational shares of total
-shares_ <- flows0 %>% group_by(cbsa, educ, occ2_a) %>% summarise(opp_pop=sum(opportunity_pop), oth_pop=sum(other_pop)) %>% 
-                     mutate(opp=round(opp_pop/sum(opp_pop+oth_pop), digits=3) , oth= round(oth_pop/sum(opp_pop+oth_pop), digits=3))
+shares_ <- flows0 %>% group_by(cbsa, educ, occ2_a) %>% summarise(opp_pop=sum(opportunity_pop), oth_pop=sum(other_pop), tot_pop=sum(probs_pop))
+                     mutate(opp=round(opp_pop/sum(opp_pop+oth_pop), digits=3) , oth= round(oth_pop/sum(opp_pop+oth_pop), digits=3), sh=tot_pop/sum(tot_pop))
 
 #why 120 metros? why some missing educ levels                     
 shares <- shares_ %>% split(.$cbsa) %>% 
@@ -84,62 +90,13 @@ flows <- flows0 %>% split(.$cbsa) %>% lapply(function(d){
 json_flows <- toJSON(flows, na="null", digits=3)
 json_shares <- toJSON(shares, na="null", digits=3)
 
-occ_names <- read_csv("/home/alec/Projects/Brookings/opportunity-industries/build/data/socxwalk.csv", n_max=22) %>% spread(occ_code, Title) %>% unbox()
+#occ_names <- read_csv("/home/alec/Projects/Brookings/opportunity-industries/build/data/socxwalk.csv", n_max=22) %>% spread(occ_code, Title) %>% unbox()
+occ_names <- read_xlsx("/home/alec/Projects/Brookings/opportunity-industries/build/data/Shortened titles.xlsx", sheet="Occupations") %>%
+  mutate(occ_code = sub("-.*$", "", Code)) %>% mutate(nchar = nchar(occ_code)) %>% select(occ_code, Title=`Short title`) %>% 
+  spread(occ_code, Title) %>% unbox()
 
 writeLines(c("var occ_flows = ", json_flows, ";", 
              "var occ_shares = ", json_shares, ";",
              "var occ_names = ", toJSON(occ_names), ";",
              "export {occ_flows, occ_shares, occ_names};"), con = "/home/alec/Projects/Brookings/opportunity-industries/build/js/occupation-data.js")
 
-
-
-
-
-
-
-
-
-
-
-#msp specific data file -- rough, need to rework code with final data
-mspflow <- read_csv("/home/alec/Projects/Brookings/opportunity-industries/build/data/flat_files/33460 Minneapolis MN-WI 3digit flows.csv")
-
-mspflow2 <- mspflow %>% filter(gender=="Total", age=="Total", race=="Total", education=="Total")
-
-wage_levels_a <- mspflow2 %>% group_by(occ_a, titlea) %>% summarise(swage=mean(p_a_wage)) %>% mutate(occ=sub("a","", occ_a))
-wage_levels_b <- mspflow2 %>% group_by(occ_b, titleb) %>% summarise(ewage=mean(f_a_wage)) %>% mutate(occ=sub("b","", occ_b))
-
-wage_levels <- full_join(wage_levels_a, wage_levels_b) %>% ungroup() %>% select(occ, title=titlea, swage, ewage)
-
-mspoccs <- occs2 %>% filter(cbsa_code==33460) %>% ungroup() %>% 
-                     select("occ", "occupation", "shtot", "total", "good_share","promising_share",
-                            "hi_good_share", "hi_promising_share", "other_share", "undefined")
-
-
-msp_summary <- full_join(mspoccs, wage_levels)
-mspflow3 <- mspflow2 %>% select(occ_a, occ_b, opportunity, other)
-
-json <- toJSON(list(summary=msp_summary, flows=mspflow3), na="null", digits=5, pretty=TRUE)
-writeLines(json, con="/home/alec/Projects/Brookings/opportunity-industries/data/msp.json")
-
-##old
-
-
-#to json
-#data questions: any differences b/n occa/titlea and b?
-#redo so that start occs is a single array of data -- avoid repeating wage data 95 times for each occ -- not sure if kosher
-dict <- mspflow %>% select(occ_a, titlea) %>% 
-          as.data.frame %>% unique() %>% 
-          filter(!duplicated(occ_a)) %>% 
-          spread(occ_a, titlea)
-
-dat <- mspflow2 %>% select(occ_a, occ_b, sw=p_a_wage, ew=f_a_wage, opp=opportunity, oth=other) %>% as.data.frame() %>% split(.$occ_a)
-
-sum_probs <- mspflow2 %>% group_by(occ_a, titlea) %>% summarise(share=sum(probs), min=min(p_a_wage), max=max(p_a_wage)) %>% mutate(diff=max-min)
-
-json <- toJSON(dat, na="null", digits=5, pretty=TRUE)
-
-writeLines(json, con="/home/alec/Projects/Brookings/opportunity-industries/data/msp.json")
-
-writeLines(c("var start_occs = ", toJSON(dict, pretty=TRUE), ";", "export default start_occs;",""), 
-           con="/home/alec/Projects/Brookings/opportunity-industries/build/js/start-occs.js")
